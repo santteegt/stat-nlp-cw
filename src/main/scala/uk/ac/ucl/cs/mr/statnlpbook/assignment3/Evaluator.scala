2f8a2abc4f72c53d5f11d26a65a85ba77c2e5f26
Sergiu Tripon <contact@sergiu-tripon.com>
2016-01-31 17:15:45 +0000

    Main.scala and Evaluator.scala

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Evaluator.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Evaluator.scala
@@ -4,14 +4,34 @@ package uk.ac.ucl.cs.mr.statnlpbook.assignment3
  * @author rockt
  */
 object Evaluator {
-  def apply(model: Model, corpus: String): Double = {
+  def apply(model: Model, corpus: String, model_p: Int, iter_p: Int, print: Int = 0): Double = {
     val total = SentimentAnalysisCorpus.numExamples(corpus)
     var correct = 0.0
     for (i <- 0 until total) {
       val (sentence, target) = SentimentAnalysisCorpus.getExample(corpus)
       val predict = model.predict(sentence)
       if (target == predict) correct = correct + 1
+
+      var prediction = 0
+
+      if (predict.equals(true)) {
+        prediction = 1
+      } else if (predict.equals(false)) {
+        prediction = 0
+      }
+
+      if (print == 1) {
+        if (model_p == 1 & iter_p == 53) {
+          scala.tools.nsc.io.File(s"./data/assignment3/predictions_model_$model_p-$iter_p.txt").appendAll(s"$prediction\n")
+        } else if (model_p == 2 && iter_p == 71) {
+          scala.tools.nsc.io.File(s"./data/assignment3/predictions_model_$model_p-$iter_p.txt").appendAll(s"$prediction\n")
+        } else if (model_p == 3 && iter_p == 91) {
+          scala.tools.nsc.io.File(s"./data/assignment3/predictions_model_$model_p-$iter_p.txt").appendAll(s"$prediction\n")
+        }
+      }
+
     }
+
     correct / total
   }
 }
--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Main.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Main.scala
@@ -9,42 +9,44 @@ object Main extends App {
    *
    * Problems 2/3/4: perform a grid search over the parameters below
    */
-//  val learningRate = 0.01
-//  val vectorRegularizationStrength = 0.01
+  //val learningRate = 0.01
+  //val vectorRegularizationStrength = 0.01
   val learningRate = 0.01 //big value yield less epochs
-  val vectorRegularizationStrength = 0.03 // best valua until now
+  val vectorRegularizationStrength = 0.01 // best value until now
   val matrixRegularizationStrength = 0.0
   val wordDim = 10
   val hiddenDim = 10
 
   val trainSetName = "train"
-//  val trainSetName = "debug"
+  //val trainSetName = "debug"
   val validationSetName = "dev"
 
-  val model: Model = new SumOfWordVectorsModel(wordDim, vectorRegularizationStrength)
+  //val model: Model = new SumOfWordVectorsModel(wordDim, vectorRegularizationStrength)
   //val model: Model = new RecurrentNeuralNetworkModel(wordDim, hiddenDim, vectorRegularizationStrength, matrixRegularizationStrength)
+  val model: Model = new OurOwnModel(wordDim, hiddenDim, vectorRegularizationStrength, matrixRegularizationStrength)
 
   def epochHook(iter: Int, accLoss: Double): Unit = {
     println("Epoch %4d\tLoss %8.4f\tTrain Acc %4.2f\tDev Acc %4.2f".format(
-      iter, accLoss, 100 * Evaluator(model, trainSetName), 100*Evaluator(model, validationSetName)))
+      iter, accLoss, 100 * Evaluator(model, trainSetName, model_p = 0, iter_p = 0, print = 0), 100 * Evaluator(model, validationSetName, model_p = 1, iter_p = iter, print = 0)))
   }
 
-  val timer = System.currentTimeMillis();
+  val timer = System.currentTimeMillis()
   StochasticGradientDescentLearner(model, trainSetName, 100, learningRate, epochHook)
-  val timeElapsed = (System.currentTimeMillis() - timer)/1000.00;
+  val timeElapsed = (System.currentTimeMillis() - timer)/1000.00
   println("Process time elapsed: %.2f seconds".format(timeElapsed))
-
-
 
   /**
    * Comment this in if you want to look at trained parameters
    */
+
   /*
   for ((paramName, paramBlock) <- model.vectorParams) {
-    println(s"$paramName:\n${paramBlock.param}\n")
+    scala.tools.nsc.io.File("./data/assignment3/tsne.txt").appendAll(s"${paramName.take(5000)}, ${paramBlock.param}\n")
+
   }
   for ((paramName, paramBlock) <- model.matrixParams) {
     println(s"$paramName:\n${paramBlock.param}\n")
   }
   */
+
 }
